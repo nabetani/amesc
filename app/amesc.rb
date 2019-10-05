d8f4910f1ef142b9b2ab2af7ee5948c2a463eb3c
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-10-05 21:18:19 +0900

    CSSからリンクしている画像を取得

--- app/amesc.rb
+++ app/amesc.rb
@@ -37,27 +37,39 @@ class Amesc
     File.open(dest, 'w') { |f| f.write(body) }
   end
 
-  def local_img_path(uri)
+  def local_path(uri, dir)
     ext = File.extname(uri.path).downcase
     body = Digest::SHA256.hexdigest(uri.to_s)
-    File.join(dest, 'img', "#{body}#{ext}")
+    File.join(dest, dir, "#{body}#{ext}")
   end
 
-  def get_image(uri, scheme, host)
+  def get_file(uri, dir, scheme, host)
     uri.scheme ||= scheme
     uri.host ||= host
     uri.query = nil
-    img_path = local_img_path(uri)
-    download(uri, img_path)
-    img_path
+    path = local_path(uri, dir)
+    download(uri, path)
+    path
+  end
+
+  def get_image(uri, scheme, host)
+    get_file(uri, 'img', scheme, host)
+  end
+
+  def get_css(uri, scheme, host)
+    get_file(uri, 'css', scheme, host)
+  end
+
+  def image?(uri)
+    ext = File.extname(uri.path).downcase
+    %w[.jpg .gif .png .jpeg].include?(ext)
   end
 
   def get_images(doc, scheme, host)
     doc.xpath('//img').each do |img|
       src = img['src']
       img_uri = URI.parse(src)
-      ext = File.extname(img_uri.path).downcase
-      next unless %w[.jpg .gif .png .jpeg].include?(ext)
+      next unless image?(img_uri)
 
       img_path = get_image(img_uri, scheme, host)
       img['src'] = File.relative_path(dest, img_path)
@@ -68,14 +80,36 @@ class Amesc
     %w[stylesheet].include?(rel)
   end
 
+  def get_css_images(uri, path)
+    mod = lambda do |all, ustr|
+      u = URI.parse(ustr)
+      if u.absolute? && image?(u)
+        e_path = get_image(u, uri.scheme, uri.host)
+        %!url("#{File.relative_path(path, e_path)}")!
+      else
+        all
+      end
+    end
+    s = File.open(path, &:read)
+    s.gsub!(/url\s*\(\s*\"([^\"]+)\"\s*\)/) do |all|
+      mod[all, Regexp.last_match(1)]
+    end
+    s.gsub!(/url\s*\(\s*\'([^\']+)\'\s*\)/) do |all|
+      mod[all, Regexp.last_match(1)]
+    end
+    File.open(path, "w") do |f|
+      f.write(s)
+    end
+  end
+
   def get_links(doc, scheme, host)
     doc.xpath('//link').each do |link|
       next unless download_rel?(link['rel'])
 
       uri = URI.parse(link['href'])
-      # <link rel="stylesheet" type="text/css" href="https://stat100.ameba.jp/ameblo/pc/css/templateStd-1.23.0.css">
-      path = get_image(uri, scheme, host)
+      path = get_css(uri, scheme, host)
       link['href'] = File.relative_path(dest, path)
+      get_css_images(uri, path)
     end
   end
 
