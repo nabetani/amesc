24481b9541baf75bdfc37d97b643dcec178638b4
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-10-05 16:52:41 +0900

    rubocop -a

--- app/main.rb
+++ app/main.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 require 'pry'
 
-require "./amesc"
+require './amesc'
 
 HERE = File.split(__FILE__)[0]
 
 def main(src_root, from, to)
-  amesc = Amesc.new(src_root, File.join( HERE, "../pages" ) )
+  amesc = Amesc.new(src_root, File.join(HERE, '../pages'))
   (from..to).each do |num|
     amesc.get_page(num)
   end
 end
 
-main( "https://"+ARGV[0], ARGV[1].to_i, ARGV[2].to_i )
+main('https://' + ARGV[0], ARGV[1].to_i, ARGV[2].to_i)
